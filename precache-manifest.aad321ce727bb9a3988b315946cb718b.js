ebf62663c7c0f7f461982eae219f7d03c124a874
elsyng <amstel01@gmail.com>
2020-05-27 17:33:57 +0200

    first commit

---
+++ precache-manifest.aad321ce727bb9a3988b315946cb718b.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0c49a33d88cbb78419156d2890d5b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65a4e1f296c4e992a6a2",
+    "url": "/static/css/main.faf72e2b.chunk.css"
+  },
+  {
+    "revision": "d10f28be8e7447fe11e8",
+    "url": "/static/js/2.205a548d.chunk.js"
+  },
+  {
+    "revision": "1384dd1b41e9db9f6d860c934f9aafb7",
+    "url": "/static/js/2.205a548d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65a4e1f296c4e992a6a2",
+    "url": "/static/js/main.77fffc86.chunk.js"
+  },
+  {
+    "revision": "0fc73d024d76569856e6",
+    "url": "/static/js/runtime-main.55886483.js"
+  },
+  {
+    "revision": "442b8b7a29e98e644b89d6fe95c59410",
+    "url": "/static/media/angle-left.442b8b7a.svg"
+  },
+  {
+    "revision": "7d2976e3c2ae722e260327f010db624b",
+    "url": "/static/media/angle-right.7d2976e3.svg"
+  },
+  {
+    "revision": "6cbe66277341a4e6d4f0d9d3bc208a7a",
+    "url": "/static/media/bars.6cbe6627.svg"
+  },
+  {
+    "revision": "cc542d2d754bf1e695ff909ada3dd04d",
+    "url": "/static/media/blender.cc542d2d.svg"
+  },
+  {
+    "revision": "486e3a36c67877be92e470bc979555da",
+    "url": "/static/media/briefcase.486e3a36.svg"
+  },
+  {
+    "revision": "9e940a0480309d94bb17a620b083ddeb",
+    "url": "/static/media/broom.9e940a04.svg"
+  },
+  {
+    "revision": "c218944fec40568620161735f37ad1e9",
+    "url": "/static/media/bug.c218944f.svg"
+  },
+  {
+    "revision": "1847ffdc25907a01d9d4984456b79e36",
+    "url": "/static/media/building.1847ffdc.svg"
+  },
+  {
+    "revision": "c86c758545e806232df8cebffe6d5dee",
+    "url": "/static/media/burn.c86c7585.svg"
+  },
+  {
+    "revision": "3ecd7e95a78aad2d798b3b9642b0ac2f",
+    "url": "/static/media/bus-alt.3ecd7e95.svg"
+  },
+  {
+    "revision": "0b4ae41e539b9dd280765e708a0d83d1",
+    "url": "/static/media/car.0b4ae41e.svg"
+  },
+  {
+    "revision": "5bca231587ca9c65fb5a24cd53eb450a",
+    "url": "/static/media/chart-area.5bca2315.svg"
+  },
+  {
+    "revision": "13af8001303a7679998c8d6d4d513c6f",
+    "url": "/static/media/chart-pie.13af8001.svg"
+  },
+  {
+    "revision": "20b5af1a2ae2d4c62d00095ab823cd09",
+    "url": "/static/media/check.20b5af1a.svg"
+  },
+  {
+    "revision": "2f47fb6ccd0d76de114c9234241f4a99",
+    "url": "/static/media/code-branch.2f47fb6c.svg"
+  },
+  {
+    "revision": "a666a3dc2358b75baa206b2dd4c5a615",
+    "url": "/static/media/corbel.a666a3dc.ttf"
+  },
+  {
+    "revision": "54daf4593212cfc921659a29bfa1ddcb",
+    "url": "/static/media/couch.54daf459.svg"
+  },
+  {
+    "revision": "df78732aa6b3d4bda159527a9bcdf8ff",
+    "url": "/static/media/credit-card.df78732a.svg"
+  },
+  {
+    "revision": "a71479d7be9027428dbff17f5fdce176",
+    "url": "/static/media/dog.a71479d7.svg"
+  },
+  {
+    "revision": "2f8aee765ce444947748c9e5bc395f9d",
+    "url": "/static/media/exchange-alt.2f8aee76.svg"
+  },
+  {
+    "revision": "82f83ecc6895902e321113f3bcd103d7",
+    "url": "/static/media/exclamation-triangle.82f83ecc.svg"
+  },
+  {
+    "revision": "e60d4fa15e533eb28cd0c4afbe8bc5e0",
+    "url": "/static/media/eye-slash.e60d4fa1.svg"
+  },
+  {
+    "revision": "8bf0eb6f138749b707c1dec1c2e68146",
+    "url": "/static/media/female.8bf0eb6f.svg"
+  },
+  {
+    "revision": "d27f8ccff8d2c2612a3ccfdf9a9c0fba",
+    "url": "/static/media/hand-holding-usd.d27f8ccf.svg"
+  },
+  {
+    "revision": "0d33552af7252961e7cca991ecb76d3d",
+    "url": "/static/media/home.0d33552a.svg"
+  },
+  {
+    "revision": "52d55dec5d1f190770a358092cd1f7af",
+    "url": "/static/media/home.52d55dec.svg"
+  },
+  {
+    "revision": "15f4912dc9e37964007c75307262641a",
+    "url": "/static/media/info-circle.15f4912d.svg"
+  },
+  {
+    "revision": "4354edcb0577ae84b87354d586f7877c",
+    "url": "/static/media/leaf.4354edcb.svg"
+  },
+  {
+    "revision": "1977e2bb82d319082d753975cf4012d5",
+    "url": "/static/media/mask-gray.1977e2bb.svg"
+  },
+  {
+    "revision": "d3f017e31f8e75bd58bdc87af4959d6f",
+    "url": "/static/media/mask-orange.d3f017e3.svg"
+  },
+  {
+    "revision": "34a82cc58cef4934feb1b090d754d056",
+    "url": "/static/media/minus.34a82cc5.svg"
+  },
+  {
+    "revision": "47063215ee9f9b99f7d7bde78cc1bd3f",
+    "url": "/static/media/mobile-alt.47063215.svg"
+  },
+  {
+    "revision": "d2dc31ee781576695d246dc10ba43e17",
+    "url": "/static/media/money-bill.d2dc31ee.svg"
+  },
+  {
+    "revision": "30c23725e9324a78481ef161e7c4f2ad",
+    "url": "/static/media/mug-hot.30c23725.svg"
+  },
+  {
+    "revision": "b76ceb04d8fad5bbb79b5e096bd16e5e",
+    "url": "/static/media/network-wired-gray.b76ceb04.svg"
+  },
+  {
+    "revision": "a119dc1b8b3fa6b2481ba58ddd66bb5d",
+    "url": "/static/media/network-wired-orange.a119dc1b.svg"
+  },
+  {
+    "revision": "8a7e67579c9ff7df0c5877571b4cdc46",
+    "url": "/static/media/notes-medical.8a7e6757.svg"
+  },
+  {
+    "revision": "b56f176bf6799b0fede612e9645d842c",
+    "url": "/static/media/passport.b56f176b.svg"
+  },
+  {
+    "revision": "a539b99845bba623b5f14e54e69b730a",
+    "url": "/static/media/piggy-bank.a539b998.svg"
+  },
+  {
+    "revision": "098b6c16106e71f192c7f55d2afc60eb",
+    "url": "/static/media/question-circle.098b6c16.svg"
+  },
+  {
+    "revision": "1060b8ee944dedb5047cf5338a14b4ad",
+    "url": "/static/media/question-circle.1060b8ee.svg"
+  },
+  {
+    "revision": "6dd9c0e3565df9ea17389d25c80e0ae5",
+    "url": "/static/media/save.6dd9c0e3.svg"
+  },
+  {
+    "revision": "926b84217878ca09e2bf8519db8ad606",
+    "url": "/static/media/shopping-basket.926b8421.svg"
+  },
+  {
+    "revision": "c56f3c8ea70769ae364ee311bcf5a4e2",
+    "url": "/static/media/suitcase-rolling.c56f3c8e.svg"
+  },
+  {
+    "revision": "efa79e02dfc4bdc6f262b569bff541c3",
+    "url": "/static/media/tag.efa79e02.svg"
+  },
+  {
+    "revision": "41566ddb135e955ecc30e5cc1703f618",
+    "url": "/static/media/times-circle.41566ddb.svg"
+  },
+  {
+    "revision": "1e03a9c3a07882781da4ba550156d05b",
+    "url": "/static/media/times.1e03a9c3.svg"
+  },
+  {
+    "revision": "c07836ef3e8aca7eb929a24f54d89ded",
+    "url": "/static/media/tooth.c07836ef.svg"
+  },
+  {
+    "revision": "2930c7369f175d423141a84991d1c1d1",
+    "url": "/static/media/tshirt.2930c736.svg"
+  },
+  {
+    "revision": "1d2f1f97a6d06810d1884f45d05b0956",
+    "url": "/static/media/user-alt.1d2f1f97.svg"
+  },
+  {
+    "revision": "af037e69176830a1efd17234bc6e10be",
+    "url": "/static/media/utensils.af037e69.svg"
+  },
+  {
+    "revision": "342c59fa13d8690b15e098a79f601906",
+    "url": "/static/media/wifi.342c59fa.svg"
+  }
+]);
